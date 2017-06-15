a2722c3005cbbe2ba488bac56651162854b9ae03
Daniel Haehn <haehn@seas.harvard.edu>
2017-06-15 10:26:48 -0400

    Forgot param.

--- _web/js/sojo.js
+++ _web/js/sojo.js
@@ -3,7 +3,7 @@ var S = S || {};
 S.load_synapse = function() {
   
   var xhr = new XMLHttpRequest();
-  xhr.open('../?image');
+  xhr.open('GET', '../?image');
   xhr.onload = S.synapse_loaded.bind(xhr);
   xhr.responseType = 'arraybuffer';
   xhr.send(null);
